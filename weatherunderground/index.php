66e178f07782bec13919e10dff2b5a3c559c06b2
Raffaello Di Martino IZ0QWM <42165424+iz0qwm@users.noreply.github.com>
2020-02-04 00:16:51 +0100

    Now rain accumulated is present

--- weatherunderground/index.php
+++ weatherunderground/index.php
@@ -15,6 +15,8 @@
 	#
 	# 	v1.0 - Feb 02, 2020
 	# 		- initial release	
+	#   v1.1 - Feb 03, 202
+	#		- accumulated rain
 	############################################################################
 	
 	# Create e new weather station on Wunderground. Keep note of Station_ID and Station_Key
@@ -44,8 +46,20 @@
 	$f_in_hpa = 33.86;
 	$f_in_mm = 25.4;
 	$pa_in_hpa = 0.01;
+	$mm_in_f = 0.03937008;
 	
-	
+	# Reading accumulated rain file
+	$read_acc_rain = "./accumulated_rain.txt";
+	if (!file_exists($read_acc_rain)) {	
+		$result_acc_rain = 0;
+	} else { 
+		$file = fopen($read_acc_rain, 'r');
+		#while (!feof($file)){ 
+			$result_acc_rain = fgets($file);
+			$result_acc_rain = round((double)$result_acc_rain, 2);
+		#}
+		fclose($file);
+	}
 	# Convert data
     # Temps
     @$weather_data['tempc'] = round( $weather_data['temperature'] - 273.15, 2 );
@@ -54,6 +68,8 @@
 	@$weather_data['dewptf'] = round( ($weather_data['dewptc'] * 1.8) + 32, 2 );
     
     # Distances
+	@$weather_data['dailyrainmm'] = $weather_data['rain'] + $result_acc_rain ;
+	@$weather_data['dailyrainin'] = $weather_data['dailyrainmm'] * $mm_in_f ;
     #@$weather_data['rainmm'] = round( $weather_data['rainin'] * $f_in_mm, 2 );
     #@$weather_data['dailyrainmm'] = round( $weather_data['dailyrainin'] * $f_in_mm, 2 );
     #@$weather_data['weeklyrainmm'] = round( $weather_data['weeklyrainin'] * $f_in_mm, 2 );
@@ -68,10 +84,14 @@
     #@$weather_data['baromrelhpa'] = round( $weather_data['baromrelin'] * $f_in_hpa, 2 );
     
     # Date and time
+	#$weather_data['dateutc'] = gmdate("Y-m-d\TH:i:s\Z");
     #$weather_data['dayutc'] = gmdate("Y-m-d\T");
-	#$weather_data['hoursutc'] = gmdate("H\Z");
+	#$weather_data['hoursutc'] = gmdate("H");
 	#$weather_data['minutesutc'] = gmdate("i\Z");
-	#$weather_data['secondsutc'] = gmdate("i\Z");
+	$time = new DateTime('NOW');
+	$h=$time->format('H');
+	$m=$time->format('i');
+	#$weather_data['secondsutc'] = gmdate("s\Z");
 	#$weather_data['dateutc'] = $weather_data['dayutc'] . "+" . $weather_data['hoursutc'] . "%3A" . $weather_data['minutesutc'] . "%3A" . $weather_data['secondsutc'];
 	#  https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=ISCAND6&PASSWORD=kgFHu4aa&dateutc=now&tempf=70&baromin=29.1&dewptf=68.2&humidity=90&softwaretype=meteohelix%20version01&action=updateraw
 	
@@ -88,11 +108,33 @@
 		@$weather_data_forward['baromin'] = $weather_data['baromrelf'] ;
 		@$weather_data_forward['dewptf'] = $weather_data['dewptf'] ;
 		@$weather_data_forward['solarradiation'] = $weather_data['irradiation'] ;
-		
+		@$weather_data_forward['dailyrainin'] = $weather_data['dailyrainin'] ;
 		
 		#@$weather_data['forward_url'] = "http://" . $forward_server . $_SERVER[REQUEST_URI];
 		@$weather_data_forward['forward_url'] = "http://" . $forward_server ;
-		@$weather_data_forward['forward'] = file_get_contents($weather_data_forward['forward_url'] . "?" . "ID=" . @$weather_data_forward['wu_id'] . "&PASSWORD=" . @$weather_data_forward['wu_station_key'] . "&dateutc=" . @$weather_data_forward['dateutc'] . "&tempf=" . @$weather_data_forward['tempf'] . "&baromin=" . @$weather_data_forward['baromin'] ."&humidity=" . @$weather_data_forward['humidity'] . "&dewptf=" . @$weather_data_forward['dewptf'] . "&solarradiation=" . @$weather_data_forward['solarradiation'] . "&softwaretype=" . @$weather_data_forward['softwaretype'] . "&action=" . @$weather_data_forward['action'] );
+		@$weather_data_forward['forward'] = file_get_contents($weather_data_forward['forward_url'] . "?" . "ID=" . @$weather_data_forward['wu_id'] . "&PASSWORD=" . @$weather_data_forward['wu_station_key'] . "&dateutc=" . @$weather_data_forward['dateutc'] . "&tempf=" . @$weather_data_forward['tempf'] . "&baromin=" . @$weather_data_forward['baromin'] ."&humidity=" . @$weather_data_forward['humidity'] . "&dewptf=" . @$weather_data_forward['dewptf'] . "&solarradiation=" . @$weather_data_forward['solarradiation'] . "&dailyrainin=" . @$weather_data_forward['dailyrainin'] . "&softwaretype=" . @$weather_data_forward['softwaretype'] . "&action=" . @$weather_data_forward['action'] );
+	}
+	
+	#
+	# Writing accumulated rain file
+	#
+	$write_acc_rain = "./accumulated_rain.txt";
+    $file = fopen($write_acc_rain, 'w');
+	$stringa = round($weather_data['dailyrainmm'], 2) . "\n";
+    fwrite($file, $stringa);
+    fclose($file);
+	
+	#echo $h;
+	#echo "\n";
+	#echo $m;
+	# Check if midnight =  reset file
+	if( $h == 23 && $m >= 55 && $m <= 59 || $h == 0 && $m >= 0 && $m <= 5) {  
+		print("Delete rain file \n");
+		$write_acc_rain = "./accumulated_rain.txt";
+		$file = fopen($write_acc_rain, 'w');
+		$stringa = "0\n";
+		fwrite($file, $stringa);
+		fclose($file);
 	}
 	
 
