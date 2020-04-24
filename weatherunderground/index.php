c0b249a7bb3c40b99b101f81fbaf690b8eabfc54
Raffaello Di Martino IZ0QWM <42165424+iz0qwm@users.noreply.github.com>
2020-04-24 12:36:28 +0200

    New pressure to sea level correction

--- weatherunderground/index.php
+++ weatherunderground/index.php
@@ -19,11 +19,13 @@
 	#		- accumulated rain
 	#   v2.0 - Feb 04, 2020
 	#		- simple UV calculation
+	#	v3.0 - Apr 24, 2020
+	#		- New sea level pressure calcuation: seaLevel = (hPa * pow(1 - (0.0065*altitude) / (bmp.temperature + (0.0065 * altitude) + 273.15),-5.257 ));
 	############################################################################
 	
 	# Create e new weather station on Wunderground. Keep note of Station_ID and Station_Key
 	# Configure this script with your Wunderground Station_ID and Station_Key
-	# To configure the baromhpacorrection you need to know the raw data in hPa of your Meteohelix, then add the correction to reach the value on sea-level
+	# Configure this script with the altitude of the Weather Station in meters to have the pressure corrected to the sea level.
 	# Put this script in a directory of you website i.e. http://mysite/wu/index.php
 	# Configure a Forwarder on weather.allmeteo.com - HTTP/Post - that points to this script 
 	
@@ -32,9 +34,9 @@
 	# Settings: General
 	$forward_data_wu = 1;
 	$forward_server = "weatherstation.wunderground.com/weatherstation/updateweatherstation.php";
-	$wu_id = "XXXXXX";
-	$wu_station_key = "*******";
-	$baromhpacorrection = 0;  # Correction for pressure on sea-level
+	$wu_id = "XXXXXXX";
+	$wu_station_key = "XXXXXX";
+	$altitude = 294; # Altitude of the weather station in meters.
 	
 	# Convert HTTP POST variables to json
 	#$weather_data = $_POST;
@@ -73,18 +75,12 @@
     # Distances
 	@$weather_data['dailyrainmm'] = round($weather_data['rain'] + $result_acc_rain, 2);
 	@$weather_data['dailyrainin'] = round($weather_data['dailyrainmm'] * $mm_in_f, 2);
-    #@$weather_data['rainmm'] = round( $weather_data['rainin'] * $f_in_mm, 2 );
-    #@$weather_data['dailyrainmm'] = round( $weather_data['dailyrainin'] * $f_in_mm, 2 );
-    #@$weather_data['weeklyrainmm'] = round( $weather_data['weeklyrainin'] * $f_in_mm, 2 );
-    #@$weather_data['monthlyrainmm'] = round( $weather_data['monthlyrainin'] * $f_in_mm, 2 );
-    #@$weather_data['yearlyrainmm'] = round( $weather_data['yearlyrainin'] * $f_in_mm, 2 );
-    #@$weather_data['rainratemm'] = round( $weather_data['rainratein'] * $f_in_mm, 2 );
+
     
     # Baros
     @$weather_data['baromabshpa'] = round( $weather_data['pressure'] * $pa_in_hpa, 2 );
-	@$weather_data['baromrelhpa'] = round( $weather_data['baromabshpa'] + $baromhpacorrection, 2);
+	@$weather_data['baromrelhpa'] = round(($weather_data['baromabshpa'] * pow(1 - (0.0065*$altitude) / ($weather_data['tempc'] + (0.0065 * $altitude) + 273.15),-5.257 )), 2);
 	@$weather_data['baromrelf'] = round( $weather_data['baromrelhpa'] / $f_in_hpa, 2 );
-    #@$weather_data['baromrelhpa'] = round( $weather_data['baromrelin'] * $f_in_hpa, 2 );
     
 	# UV simple calculation
 	if ( $weather_data['irradiation'] < 70 ) 
